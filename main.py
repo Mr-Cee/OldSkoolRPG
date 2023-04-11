050cca8ca498eb79e8406c52b9e44f58172fef9c
Mr_Cee <Christie.josh@live.com>
2023-04-11 12:24:42 -0800

    test commit

--- main.py
+++ main.py
@@ -9,28 +9,73 @@ class Game:
         pygame.init()
         self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
         self.clock = pygame.time.Clock()
-        self.font = pygame.font.Font('Arial', 32)
         self.running = True
 
+    def createTilemap(self):
+        for i, row in enumerate(tilemap):
+            for j, column in enumerate(row):
+                if column == "B":
+                    Block(self, j, i)
+                if column == "P":
+                    Player(self, j, i)
+
+
     def new(self):
+
         # a new game starts
         self.playing = True
 
         self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.blocks = pygame.sprite.LayeredUpdates()
+        self.enemies = pygame.sprite.LayeredUpdates()
+        self.attacks = pygame.sprite.LayeredUpdates()
+
+        self.createTilemap()
+
+
+    def events(self):
+        # Code for watching events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+            # if event.type == pygame.MOUSEBUTTONDOWN:
+            #     self.player.set_target(pygame.mouse.get_pos())
 
     def update(self):
         # Code for updating game
+        self.all_sprites.update()
 
     def draw(self):
         # Code for drawing to screen
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        self.clock.tick(FPS)
+        pygame.display.update()
 
     def main(self):
         # Main Game Loop
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        self.running = False
 
     def game_over(self):
         # Code for game over conditions
+        pass
 
     def intro_screen(self):
         # Code for showing introduction screen
+        pass
 
 
+g = Game()
+g.intro_screen()
+g.new()
+while g.running:
+    g.main()
+    g.game_over()
+
+pygame.quit()
+sys.exit()
---
+++ sprites.py
@@ -0,0 +1,91 @@
+import pygame
+from config import *
+import math
+import random
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.game = game
+        self._layer = PLAYER_LAYER
+        self.groups = self.game.all_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
+        self.width = TILESIZE
+        self.height = TILESIZE
+
+        self.x_change = 0
+        self.y_change = 0
+
+        self.facing = 'down'
+
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(RED)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+        # self.pos = pygame.Vector2(x, y)
+        # self.set_target((x, y))
+
+    # def set_target(self, pos):
+    #     self.target = pygame.Vector2(pos)
+
+    def update(self):
+        self.movement()
+
+        self.rect.x += self.x_change
+        self.rect.y += self.y_change
+
+        self.x_change = 0
+        self.y_change = 0
+
+        # move = self.target - self.pos
+        # move_length = move.length()
+        #
+        # if move_length < PLAYER_SPEED:
+        #     self.pos = self.target
+        # elif move_length != 0:
+        #     move.normalize_ip()
+        #     move = move * PLAYER_SPEED
+        #     self.pos += move
+        #
+        # self.rect.midbottom = list(int(v) for v in self.pos)
+
+    def movement(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.x_change -= PLAYER_SPEED
+            self.facing = 'left'
+        if keys[pygame.K_RIGHT]:
+            self.x_change += PLAYER_SPEED
+            self.facing = 'right'
+        if keys[pygame.K_UP]:
+            self.y_change -= PLAYER_SPEED
+            self.facing = 'up'
+        if keys[pygame.K_DOWN]:
+            self.y_change += PLAYER_SPEED
+            self.facing = 'down'
+
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.game = game
+        self._layer = BLOCK_LAYER
+        self.groups = self.game.all_sprites, self.game.blocks
+        pygame.sprite.Sprite.__init__(self, self.groups)
+
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
+        self.width = TILESIZE
+        self.height = TILESIZE
+
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(BLUE)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
