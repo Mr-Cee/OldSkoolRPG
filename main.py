6247eeb70059fc801477217d02b371fb2a47ce63
Mr_Cee <Christie.josh@live.com>
2023-04-11 08:58:29 -0800

    test commit

--- main.py
+++ main.py
@@ -1,37 +1,41 @@
 import pygame
-
-pygame.init()
-
-width = 800
-height = 800
-game_window_bg = (255, 255, 255)
-
-game_window = pygame.display.set_mode((width, height))
-
-clock = pygame.time.Clock()
-
-# Test Push
-
-# Test Pull
+from sprites import *
+from config import *
+import sys
 
 
-def run_game_loop():
-    while True:
-        # Handle Events
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.QUIT:
-                return
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.Font('Arial', 32)
+        self.running = True
 
-        # Execute Logic
-        # Update Display
-        game_window.fill(game_window_bg)
-        pygame.display.update()
+    def new(self):
+        # a new game starts
+        self.playing = True
 
-        clock.tick(60)
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+
+    def update(self):
+        # Todo
+        #Code for updating game
+
+    def draw(self):
+        # Todo
+        # Code for drawing to screen
+
+    def main(self):
+        # Todo
+        # Main Game Loop
+
+    def game_over(self):
+        # Todo
+        # Code for game over conditions
+
+    def intro_screen(self):
+        # Todo
+        # Code for showing introduction screen
 
 
-run_game_loop()
-
-pygame.quit()
-quit()
